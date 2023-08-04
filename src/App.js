48806fde3c8d14fb028f61f29e3a53b1fe45afff
ksanni <ksanni58627@gmail.com>
2023-08-04 12:27:44 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter,Routes, Route} from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+import Details from './components/Details/Details';
+import Navbar1 from './components/Navbar/Navbar1';
+import Cart from './components/Cart/Cart';
+import Payment from './components/Pyament/Payment';
+import {AppContext} from './components/Context/Context';
+import Confirm from './components/Confirm/Confirm';
+import Sinup from './components/Profile/Sinup/Sinup';
+import Login from './components/Profile/Login/Login';
+import Navbar2 from './components/Navbar/Navbar2';
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      
+      <BrowserRouter>
+      <AppContext>
+      <Navbar1/> 
+      <Navbar2/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/sinup' element={<Sinup/>}/>
+          <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/details/:id' element={<Details/>}/>
+          <Route exact path='/cart' element={<Cart/>}/>
+          <Route exact path='/payment' element={<Payment/>}/>
+          <Route exact path='/confirm' element={<Confirm/>}/>
+         
+        </Routes>
+        </AppContext>
+        </BrowserRouter>
+     </>
+  )
 }
+
 
 export default App;
